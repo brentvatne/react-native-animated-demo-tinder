eed7445fdd0c6f0a9572ea03a4b3330999736205
Brent Vatne <brentvatne@gmail.com>
2015-07-08 19:53:36 -0700

    Initial commit

---
+++ README.markdown
@@ -0,0 +1,4 @@
+Simple example of Tinder-style gestures/animations using the new
+Animated API in React Native
+
+![](http://url.brentvatne.ca/16ylP.gif)
---
+++ index.ios.js
@@ -0,0 +1,183 @@
+'use strict';
+
+import React, { AppRegistry, StyleSheet, Text, View, Animated, Component, PanResponder, Image, } from 'react-native';
+import clamp from 'clamp';
+
+// Network images appear to be broken
+//
+// const People = [
+//   'http://avatars1.githubusercontent.com/u/90494?v=3&s=460',
+//   'http://avatars2.githubusercontent.com/u/56719?v=3&s=400',
+//   'http://avatars0.githubusercontent.com/u/379606?v=3&s=400',
+//   'http://avatars3.githubusercontent.com/u/2922932?v=3&s=400',
+// ]
+
+const People = [
+  'red',
+  'green',
+  'blue',
+  'purple',
+  'orange',
+]
+
+class Flix extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      pan: new Animated.ValueXY(),
+      enter: new Animated.Value(0.5),
+      person: People[0],
+    }
+  }
+
+  _goToNextPerson() {
+    let currentPersonIdx = People.indexOf(this.state.person);
+    let newIdx = currentPersonIdx + 1;
+
+    this.setState({
+      person: People[newIdx > People.length - 1 ? 0 : newIdx]
+    });
+  }
+
+  componentDidMount() {
+    this._animateEntrance();
+  }
+
+  _animateEntrance() {
+    Animated.spring(
+      this.state.enter,
+      { toValue: 1, friction: 8 }
+    ).start();
+  }
+
+  componentWillMount() {
+    this._panResponder = PanResponder.create({
+      onMoveShouldSetResponderCapture: () => true,
+      onMoveShouldSetPanResponderCapture: () => true,
+
+      onPanResponderGrant: (e, gestureState) => {
+        this.state.pan.setOffset({x: this.state.pan.x._value, y: this.state.pan.y._value});
+        this.state.pan.setValue({x: 0, y: 0});
+      },
+
+      onPanResponderMove: Animated.event([
+        null, {dx: this.state.pan.x, dy: this.state.pan.y},
+      ]),
+
+      onPanResponderRelease: (e, {vx, vy}) => {
+        this.state.pan.flattenOffset();
+        var velocity;
+
+        if (vx > 0) {
+          velocity = clamp(vx, 3, 5);
+        } else if (vx < 0) {
+          velocity = clamp(vx * -1, 3, 5) * -1;
+        }
+
+        if (this.state.pan.x._value > 120 || this.state.pan.x._value < -120) {
+          Animated.decay(this.state.pan.x, {
+            velocity: velocity,
+            deceleration: 0.98,
+          }).start(this._resetState.bind(this))
+
+          Animated.decay(this.state.pan.y, {
+            velocity: vy,
+            deceleration: 0.985,
+          }).start();
+        } else {
+          Animated.spring(this.state.pan, {
+            toValue: {x: 0, y: 0},
+            friction: 4
+          }).start()
+        }
+      }
+    })
+  }
+
+  _resetState() {
+    this.state.pan.setValue({x: 0, y: 0});
+    this.state.enter.setValue(0);
+    this._goToNextPerson();
+    this._animateEntrance();
+  }
+
+  render() {
+    let { pan, enter, } = this.state;
+
+    let [translateX, translateY] = [pan.x, pan.y];
+
+    let rotate = pan.x.interpolate({inputRange: [-200, 0, 200], outputRange: ["-30deg", "0deg", "30deg"]});
+    let opacity = pan.x.interpolate({inputRange: [-200, 0, 200], outputRange: [0.5, 1, 0.5]})
+    let scale = enter;
+
+    let animatedCardStyles = {transform: [{translateX}, {translateY}, {rotate}, {scale}], opacity};
+
+    let yupOpacity = pan.x.interpolate({inputRange: [0, 150], outputRange: [0, 1]});
+    let yupScale = pan.x.interpolate({inputRange: [0, 150], outputRange: [0.5, 1], extrapolate: 'clamp'});
+    let animatedYupStyles = {transform: [{scale: yupScale}], opacity: yupOpacity}
+
+    let nopeOpacity = pan.x.interpolate({inputRange: [-150, 0], outputRange: [1, 0]});
+    let nopeScale = pan.x.interpolate({inputRange: [-150, 0], outputRange: [1, 0.5], extrapolate: 'clamp'});
+    let animatedNopeStyles = {transform: [{scale: nopeScale}], opacity: nopeOpacity}
+
+    return (
+      <View style={styles.container}>
+        <Animated.View style={[styles.card, animatedCardStyles, {backgroundColor: this.state.person}]} {...this._panResponder.panHandlers}>
+        </Animated.View>
+
+        <Animated.View style={[styles.nope, animatedNopeStyles]}>
+          <Text style={styles.nopeText}>Nope!</Text>
+        </Animated.View>
+
+        <Animated.View style={[styles.yup, animatedYupStyles]}>
+          <Text style={styles.yupText}>Yup!</Text>
+        </Animated.View>
+      </View>
+    );
+  }
+}
+
+// <Image source={{uri: this.state.person}} style={{width: 200, height: 200}} resizeMode='cover' />
+
+var styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  card: {
+    width: 200,
+    height: 200,
+    backgroundColor: 'red',
+  },
+  yup: {
+    borderColor: 'green',
+    borderWidth: 2,
+    position: 'absolute',
+    padding: 20,
+    bottom: 20,
+    borderRadius: 5,
+    right: 20,
+  },
+  yupText: {
+    fontSize: 16,
+    color: 'green',
+  },
+  nope: {
+    borderColor: 'red',
+    borderWidth: 2,
+    position: 'absolute',
+    bottom: 20,
+    padding: 20,
+    borderRadius: 5,
+    left: 20,
+  },
+  nopeText: {
+    fontSize: 16,
+    color: 'red',
+  }
+});
+
+AppRegistry.registerComponent('Flix', () => Flix);
